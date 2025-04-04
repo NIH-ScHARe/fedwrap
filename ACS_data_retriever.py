6024f93ba78ea0e8b8488de539f7a5affae4c0f5
mark-NIH <mark.aronson@nih.gov>
2025-04-04 13:54:38 -0400

    create ACS_data_retriever
    broke out dataset-specific functions from general functions, added ability to parse data by year correctly

---
+++ ACS_data_retriever.py
@@ -0,0 +1,272 @@
+from census_API_wrapper import get_demo_data
+
+def get_total_pop(year,geo):
+    
+    # set labels 
+    labels = ['Estimate!!SEX AND AGE!!Total population']
+    if year == '2009':
+        labels = ['Number!!Estimate!!SEX AND AGE!!Total population']
+   
+    return get_demo_data('DP05',year,geo,labels)
+
+def get_pop_sex_percent(year,geo):
+    
+    # set labels 
+    if year in ['2009']:
+        labels = ['Percent!!Estimate!!SEX AND AGE!!Total population!!Male',
+                  'Percent!!Estimate!!SEX AND AGE!!Total population!!Female']
+    elif year in ['2010','2011','2012']:
+        labels = ['Percent!!SEX AND AGE!!Male',
+                  'Percent!!SEX AND AGE!!Female']
+    elif year in ['2013','2014','2015','2016','2019','2020','2021','2022','2023']:
+        labels = ['Percent!!SEX AND AGE!!Total population!!Male',
+                  'Percent!!SEX AND AGE!!Total population!!Female']
+    elif year in ['2017','2018']:
+        labels = ['Percent Estimate!!SEX AND AGE!!Total population!!Male',
+                  'Percent Estimate!!SEX AND AGE!!Total population!!Female']
+    else:
+        print(f"Error: Unsupported year '{year}'")
+        return None
+    
+    return get_demo_data('DP05',year,geo,labels)
+
+def get_age_percent(year,geo):
+
+    if year in ['2009']:
+        labels = ['Percent!!Estimate!!SEX AND AGE!!Total population!!Under 5 years',
+                  'Percent!!Estimate!!SEX AND AGE!!Total population!!5 to 9 years',
+                  'Percent!!Estimate!!SEX AND AGE!!Total population!!10 to 14 years',
+                  'Percent!!Estimate!!SEX AND AGE!!Total population!!15 to 19 years',
+                  'Percent!!Estimate!!SEX AND AGE!!Total population!!20 to 24 years',
+                  'Percent!!Estimate!!SEX AND AGE!!Total population!!25 to 34 years',
+                  'Percent!!Estimate!!SEX AND AGE!!Total population!!35 to 44 years',
+                  'Percent!!Estimate!!SEX AND AGE!!Total population!!45 to 54 years',
+                  'Percent!!Estimate!!SEX AND AGE!!Total population!!55 to 59 years',
+                  'Percent!!Estimate!!SEX AND AGE!!Total population!!60 to 64 years',
+                  'Percent!!Estimate!!SEX AND AGE!!Total population!!65 to 74 years',
+                  'Percent!!Estimate!!SEX AND AGE!!Total population!!75 to 84 years',
+                  'Percent!!Estimate!!SEX AND AGE!!Total population!!85 years and over']
+    elif year in ['2010','2011','2012','2013','2014','2015','2016']:
+        labels = ['Percent!!SEX AND AGE!!Under 5 years',
+                  'Percent!!SEX AND AGE!!5 to 9 years',
+                  'Percent!!SEX AND AGE!!10 to 14 years',
+                  'Percent!!SEX AND AGE!!15 to 19 years',
+                  'Percent!!SEX AND AGE!!20 to 24 years',
+                  'Percent!!SEX AND AGE!!25 to 34 years',
+                  'Percent!!SEX AND AGE!!35 to 44 years',
+                  'Percent!!SEX AND AGE!!45 to 54 years',
+                  'Percent!!SEX AND AGE!!55 to 59 years',
+                  'Percent!!SEX AND AGE!!60 to 64 years',
+                  'Percent!!SEX AND AGE!!65 to 74 years',
+                  'Percent!!SEX AND AGE!!75 to 84 years',
+                  'Percent!!SEX AND AGE!!85 years and over']
+    elif year in ['2017','2018']:
+        labels = ['Percent Estimate!!SEX AND AGE!!Total population!!Under 5 years',
+                  'Percent Estimate!!SEX AND AGE!!Total population!!5 to 9 years',
+                  'Percent Estimate!!SEX AND AGE!!Total population!!10 to 14 years',
+                  'Percent Estimate!!SEX AND AGE!!Total population!!15 to 19 years',
+                  'Percent Estimate!!SEX AND AGE!!Total population!!20 to 24 years',
+                  'Percent Estimate!!SEX AND AGE!!Total population!!25 to 34 years',
+                  'Percent Estimate!!SEX AND AGE!!Total population!!35 to 44 years',
+                  'Percent Estimate!!SEX AND AGE!!Total population!!45 to 54 years',
+                  'Percent Estimate!!SEX AND AGE!!Total population!!55 to 59 years',
+                  'Percent Estimate!!SEX AND AGE!!Total population!!60 to 64 years',
+                  'Percent Estimate!!SEX AND AGE!!Total population!!65 to 74 years',
+                  'Percent Estimate!!SEX AND AGE!!Total population!!75 to 84 years',
+                  'Percent Estimate!!SEX AND AGE!!Total population!!85 years and over']
+    elif year in ['2019','2020','2021','2022','2023']:
+        labels = ['Percent!!SEX AND AGE!!Total population!!Under 5 years',
+                  'Percent!!SEX AND AGE!!Total population!!5 to 9 years',
+                  'Percent!!SEX AND AGE!!Total population!!10 to 14 years',
+                  'Percent!!SEX AND AGE!!Total population!!15 to 19 years',
+                  'Percent!!SEX AND AGE!!Total population!!20 to 24 years',
+                  'Percent!!SEX AND AGE!!Total population!!25 to 34 years',
+                  'Percent!!SEX AND AGE!!Total population!!35 to 44 years',
+                  'Percent!!SEX AND AGE!!Total population!!45 to 54 years',
+                  'Percent!!SEX AND AGE!!Total population!!55 to 59 years',
+                  'Percent!!SEX AND AGE!!Total population!!60 to 64 years',
+                  'Percent!!SEX AND AGE!!Total population!!65 to 74 years',
+                  'Percent!!SEX AND AGE!!Total population!!75 to 84 years',
+                  'Percent!!SEX AND AGE!!Total population!!85 years and over']
+    else:
+        print(f"Error: Unsupported year '{year}'")
+        return None
+    
+    
+    return get_demo_data('DP05',year,geo,labels)
+
+def get_race_percent(year,geo):
+
+    # set new labels 
+    if year in ['2009']:
+        labels = ['Percent!!Estimate!!Race alone or in combination with one or more other races!!Total population!!White',
+                  'Percent!!Estimate!!Race alone or in combination with one or more other races!!Total population!!Black or African American',
+                  'Percent!!Estimate!!Race alone or in combination with one or more other races!!Total population!!American Indian and Alaska Native',
+                  'Percent!!Estimate!!Race alone or in combination with one or more other races!!Total population!!Asian',
+                  'Percent!!Estimate!!Race alone or in combination with one or more other races!!Total population!!Native Hawaiian and Other Pacific Islander',
+                  'Percent!!Estimate!!Race alone or in combination with one or more other races!!Total population!!Some other race']
+    elif year in ['2010','2011','2012']:
+        labels = ['Percent!!RACE!!White',
+                  'Percent!!RACE!!Black or African American',
+                  'Percent!!RACE!!American Indian and Alaska Native',
+                  'Percent!!RACE!!Asian',
+                  'Percent!!RACE!!Native Hawaiian and Other Pacific Islander',
+                  'Percent!!RACE!!Some other race']
+    elif year in ['2013','2014','2015','2016']:
+        labels = ['Percent!!RACE!!Race alone or in combination with one or more other races!!Total population!!White',
+                  'Percent!!RACE!!Race alone or in combination with one or more other races!!Total population!!Black or African American',
+                  'Percent!!RACE!!Race alone or in combination with one or more other races!!Total population!!American Indian and Alaska Native',
+                  'Percent!!RACE!!Race alone or in combination with one or more other races!!Total population!!Asian',
+                  'Percent!!RACE!!Race alone or in combination with one or more other races!!Total population!!Native Hawaiian and Other Pacific Islander',
+                  'Percent!!RACE!!Race alone or in combination with one or more other races!!Total population!!Some other race']
+    elif year in ['2017','2018']:
+        labels = ['Percent Estimate!!Race alone or in combination with one or more other races!!Total population!!White',
+                  'Percent Estimate!!Race alone or in combination with one or more other races!!Total population!!Black or African American',
+                  'Percent Estimate!!Race alone or in combination with one or more other races!!Total population!!American Indian and Alaska Native',
+                  'Percent Estimate!!Race alone or in combination with one or more other races!!Total population!!Asian',
+                  'Percent Estimate!!Race alone or in combination with one or more other races!!Total population!!Native Hawaiian and Other Pacific Islander',
+                  'Percent Estimate!!Race alone or in combination with one or more other races!!Total population!!Some other race']
+    elif year in ['2019','2020','2021','2022','2023']:
+        labels = ['Percent!!Race alone or in combination with one or more other races!!Total population!!White',
+                  'Percent!!Race alone or in combination with one or more other races!!Total population!!Black or African American',
+                  'Percent!!Race alone or in combination with one or more other races!!Total population!!American Indian and Alaska Native',
+                  'Percent!!Race alone or in combination with one or more other races!!Total population!!Asian',
+                  'Percent!!Race alone or in combination with one or more other races!!Total population!!Native Hawaiian and Other Pacific Islander',
+                  'Percent!!Race alone or in combination with one or more other races!!Total population!!Some other race']
+    else:
+        print(f"Error: Unsupported year '{year}'")
+        return None
+    
+    
+    return get_demo_data('DP05',year,geo,labels)
+
+# def get_household_type(year,geo):
+    
+#     # set labels 
+#     labels = ['ucgid','Married-couple household',
+#               'Cohabiting couple household',
+#               'Male householder, no spouse/partner present',
+#               'Female householder, no spouse/partner present']
+    
+#     return get_demo_data('DP02',year,geo,variables,labels)
+
+# def get_household_relationships(year,geo):
+    
+#     # set variables from codebook 
+#     variables = ['ucgid','DP02_0019PE','DP02_0020PE','DP02_0021PE',
+#                  'DP02_0022PE','DP02_0023PE','DP02_0024PE']
+    
+#     # set new labels 
+#     labels = ['ucgid','Householder','Spouse','Unmarried partner','Child',
+#               'Other relatives','Other nonrelatives']
+    
+#     return get_demo_data('DP02',year,geo,variables,labels)
+
+# def get_male_marital_status(year,geo):
+    
+#     # set variables from codebook 
+#     variables = ['ucgid','DP02_0026PE','DP02_0027PE','DP02_0028PE',
+#                  'DP02_0029PE','DP02_0030PE']
+    
+#     # set new labels 
+#     labels = ['ucgid','Never married','Now married, except separated',
+#               'Separated','Widowed','Divorced']
+    
+#     return get_demo_data('DP02',year,geo,variables,labels)
+
+# def get_female_marital_status(year,geo):
+    
+#     # set variables from codebook 
+#     variables = ['ucgid','DP02_0032PE','DP02_0033PE','DP02_0034PE',
+#                  'DP02_0035PE','DP02_0036PE']
+    
+#     # set new labels 
+#     labels = ['ucgid','Never married','Now married, except separated',
+#               'Separated','Widowed','Divorced']
+    
+#     return get_demo_data('DP02',year,geo,variables,labels)
+
+# def get_school_enrollment(year,geo):
+    
+#     # set variables from codebook 
+#     variables = ['ucgid','DP02_0054PE','DP02_0055PE','DP02_0056PE',
+#                  'DP02_0057PE','DP02_0058PE']
+    
+#     # set new labels 
+#     labels = ['ucgid','Nursery school, preschool','Kindergarten',
+#               'Elementary school (grades 1-8)',
+#               'High school (grades 9-12)',
+#               'College or graduate school']
+    
+#     return get_demo_data('DP02',year,geo,variables,labels)
+
+# def get_education_level(year,geo):
+    
+#     # set variables from codebook 
+#     variables = ['ucgid','DP02_0060PE','DP02_0061PE','DP02_0062PE',
+#                  'DP02_0063PE','DP02_0064PE','DP02_0065PE','DP02_0066PE']
+    
+#     # set new labels 
+#     labels = ['ucgid','Less than 9th grade','9th to 12th grade, no diploma',
+#               'High school graduate (includes equivalency)',
+#               'Some college, no degree','Associate\'s degree',
+#               'Bachelor\'s degree','Graduate or professional degree']
+    
+#     return get_demo_data('DP02',year,geo,variables,labels)
+
+# def get_veteran_status(year,geo):
+    
+#     # set variables from codebook 
+#     variables = ['ucgid','DP02_0070PE']
+    
+#     # set new labels 
+#     labels = ['ucgid','Civilian veterans']
+    
+#     return get_demo_data('DP02',year,geo,variables,labels)
+
+# def get_disability_status(year,geo):
+    
+#     # set variables from codebook 
+#     variables = ['ucgid','DP02_0072PE']
+    
+#     # set new labels 
+#     labels = ['ucgid','With a disability (%)']
+    
+#     return get_demo_data('DP02',year,geo,variables,labels)
+
+# def get_residence_year_ago(year,geo):
+    
+#     # set variables from codebook 
+#     variables = ['ucgid','DP02_0079E','DP02_0080E','DP02_0083E','DP02_0085E',
+#                  'DP02_0086E','DP02_0087E']
+    
+#     labels = ['ucgid',
+#               'Population 1 year and over',
+#               'Same house',
+#               'Different house in the U.S., Same county',
+#               'Different house in the U.S., Same state, Different county',
+#               'Different house in the U.S., Different state',
+#               'Abroad']
+
+#     abs_data = get_demo_data('DP02',year,geo,variables,labels)
+
+#     category_columns = abs_data.columns[2::]
+
+#     perc_data = abs_data[category_columns].div(abs_data['Population 1 year and over'], axis=0)
+#     perc_data = perc_data.multiply(100).round(1)
+
+#     perc_data['ucgid'] = abs_data['ucgid']
+    
+#     return perc_data
+
+# pop_data = get_total_pop('2009','state')
+# pop_sex_data = get_pop_sex_percent('2009', 'county')
+# age_data = get_age_percent('2012','state')
+race_data = get_race_percent('2015','state')
+
+# household_data = get_household_relationships('2022', 'state')
+
+# male_marital_data = get_male_marital_status('2020', 'state')
+# enrollment_data = get_education_level('2020', 'state')
+# veteran_data = get_veteran_status('2023', 'state')
+# previous_residence_data = get_residence_year_ago('2023', 'state')
--- census_API_wrapper.py
+++ census_API_wrapper.py
@@ -1,5 +1,6 @@
 import requests 
 import pandas as pd
+from bs4 import BeautifulSoup
 
 def get_api_dataframe(url):
     
@@ -22,7 +23,59 @@ def get_ACS_url(year,table,geo):
     
     return 'https://api.census.gov/data/' + year + '/acs/acs5/profile?get=group(' + table + ')&ucgid=' + geo_pseudo_map.get(geo) 
 
-def get_demo_data(table,year,geo,variables,labels):
+def get_ACS_metadata(year,table):
+    
+    # define URL
+    url = 'https://api.census.gov/data/' + year + '/acs/acs5/profile/groups/' + table + '.html'
+
+    # Fetch HTML content 
+    response = requests.get(url)
+
+    # Parse HTML 
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # Find the table 
+    table = soup.find("table")
+
+    # Extract headers 
+    headers = [th.text.strip() for th in table.find_all("th")]
+
+    # Extract rows
+    rows = []
+    for tr in table.find_all("tr")[2:]:  # Skip header and variable number rows
+        cells = [td.text.strip() for td in tr.find_all("td")]
+        rows.append(cells)
+
+    # Return DataFrame
+    return pd.DataFrame(rows, columns=headers)
+
+def get_variable_name(table,label):
+    
+    match = table.loc[table['Label'] == label, 'Name']
+    return match.iloc[0] if not match.empty else None
+
+def get_variables(table, year, labels):
+    
+    # get metadata table 
+    variable_table = get_ACS_metadata(year,table)
+    
+    # iterate through each label and return variable name from table 
+    variables = []
+    for label in labels:
+        variables.append(get_variable_name(variable_table, label))
+    
+    # return list of variable names 
+    return variables 
+
+def extract_label_name(labels):
+    
+    short_labels = ['ucgid']
+    for label in labels:
+        short_labels.append(label.split('!!')[-1])
+    
+    return short_labels
+
+def get_demo_data(table,year,geo,labels):
     
     # get url for API call 
     url = get_ACS_url(year,table,geo)
@@ -30,195 +83,21 @@ def get_demo_data(table,year,geo,variables,labels):
     # get data from API request and format into dataframe 
     all_data = get_api_dataframe(url)
 
+    # get variable names based on labels  
+    variables = get_variables(table,year,labels)
+
+    # append ucgid code 
+    variables.insert(0,'ucgid')
+
     # make a copy of the dataframe with selected variables 
     data = all_data[variables].copy()
     
     # set dataframe labels 
-    data.columns = labels
+    data.columns = extract_label_name(labels)
     
     # convert to float values 
     data[data.columns[1:]] = data[data.columns[1:]].astype(float)
     
     return data
 
-def get_total_pop(year,geo):
-    
-    # set variables from codebook 
-    variables = ['ucgid','DP05_0001E']
-    
-    # set new labels 
-    labels = ['ucgid','Total population']
-    
-    return get_demo_data('DP05',year,geo,variables,labels)
 
-def get_pop_sex_percent(year,geo):
-    
-    # set variables from codebook 
-    variables = ['ucgid','DP05_0002PE','DP05_0003PE']
-    
-    # set new labels 
-    labels = ['ucgid','Male','Female']
-    
-    return get_demo_data('DP05',year,geo,variables,labels)
-
-def get_age_percent(year,geo):
-
-    # set variables from codebook 
-    variables = ['ucgid','DP05_0005PE','DP05_0006PE','DP05_0007PE','DP05_0008PE',
-                     'DP05_0009PE','DP05_0010PE','DP05_0011PE','DP05_0012PE',
-                     'DP05_0013PE','DP05_0014PE','DP05_0015PE','DP05_0016PE',
-                     'DP05_0017PE']
-    
-    # set new labels 
-    labels = ['ucgid','under 5','5-9','10-14','15-19','20-24','25-34','35-44','45-54',
-              '55-59','60-64','65-74','75-84','85+']
-    
-    return get_demo_data('DP05',year,geo,variables,labels)
-
-def get_race_percent(year,geo):
-
-    # set variables from codebook 
-    variables = ['ucgid','DP05_0037PE','DP05_0038PE','DP05_0039PE',
-                 'DP05_0044PE','DP05_0052PE','DP05_0057PE','DP05_0058PE']
-    
-    # set new labels 
-    labels = ['ucgid','White','Black or African American',
-              'American Indian and Alaska Native','Asian',
-              'Native Hawaiian and Other Pacific Islander',
-              'Some other race','Two or more races']
-    
-    return get_demo_data('DP05',year,geo,variables,labels)
-
-def get_household_type(year,geo):
-    
-    # set variables from codebook 
-    variables = ['ucgid','DP02_0002PE','DP02_0004PE','DP02_0006PE',
-                 'DP02_0010PE']
-    
-    # set new labels 
-    labels = ['ucgid','Married-couple household',
-              'Cohabiting couple household',
-              'Male householder, no spouse/partner present',
-              'Female householder, no spouse/partner present']
-    
-    return get_demo_data('DP02',year,geo,variables,labels)
-
-def get_household_relationships(year,geo):
-    
-    # set variables from codebook 
-    variables = ['ucgid','DP02_0019PE','DP02_0020PE','DP02_0021PE',
-                 'DP02_0022PE','DP02_0023PE','DP02_0024PE']
-    
-    # set new labels 
-    labels = ['ucgid','Householder','Spouse','Unmarried partner','Child',
-              'Other relatives','Other nonrelatives']
-    
-    return get_demo_data('DP02',year,geo,variables,labels)
-
-def get_male_marital_status(year,geo):
-    
-    # set variables from codebook 
-    variables = ['ucgid','DP02_0026PE','DP02_0027PE','DP02_0028PE',
-                 'DP02_0029PE','DP02_0030PE']
-    
-    # set new labels 
-    labels = ['ucgid','Never married','Now married, except separated',
-              'Separated','Widowed','Divorced']
-    
-    return get_demo_data('DP02',year,geo,variables,labels)
-
-def get_female_marital_status(year,geo):
-    
-    # set variables from codebook 
-    variables = ['ucgid','DP02_0032PE','DP02_0033PE','DP02_0034PE',
-                 'DP02_0035PE','DP02_0036PE']
-    
-    # set new labels 
-    labels = ['ucgid','Never married','Now married, except separated',
-              'Separated','Widowed','Divorced']
-    
-    return get_demo_data('DP02',year,geo,variables,labels)
-
-def get_school_enrollment(year,geo):
-    
-    # set variables from codebook 
-    variables = ['ucgid','DP02_0054PE','DP02_0055PE','DP02_0056PE',
-                 'DP02_0057PE','DP02_0058PE']
-    
-    # set new labels 
-    labels = ['ucgid','Nursery school, preschool','Kindergarten',
-              'Elementary school (grades 1-8)',
-              'High school (grades 9-12)',
-              'College or graduate school']
-    
-    return get_demo_data('DP02',year,geo,variables,labels)
-
-def get_education_level(year,geo):
-    
-    # set variables from codebook 
-    variables = ['ucgid','DP02_0060PE','DP02_0061PE','DP02_0062PE',
-                 'DP02_0063PE','DP02_0064PE','DP02_0065PE','DP02_0066PE']
-    
-    # set new labels 
-    labels = ['ucgid','Less than 9th grade','9th to 12th grade, no diploma',
-              'High school graduate (includes equivalency)',
-              'Some college, no degree','Associate\'s degree',
-              'Bachelor\'s degree','Graduate or professional degree']
-    
-    return get_demo_data('DP02',year,geo,variables,labels)
-
-def get_veteran_status(year,geo):
-    
-    # set variables from codebook 
-    variables = ['ucgid','DP02_0070PE']
-    
-    # set new labels 
-    labels = ['ucgid','Civilian veterans']
-    
-    return get_demo_data('DP02',year,geo,variables,labels)
-
-def get_disability_status(year,geo):
-    
-    # set variables from codebook 
-    variables = ['ucgid','DP02_0072PE']
-    
-    # set new labels 
-    labels = ['ucgid','With a disability (%)']
-    
-    return get_demo_data('DP02',year,geo,variables,labels)
-
-def get_residence_year_ago(year,geo):
-    
-    # set variables from codebook 
-    variables = ['ucgid','DP02_0079E','DP02_0080E','DP02_0083E','DP02_0085E',
-                 'DP02_0086E','DP02_0087E']
-    
-    labels = ['ucgid',
-              'Population 1 year and over',
-              'Same house',
-              'Different house in the U.S., Same county',
-              'Different house in the U.S., Same state, Different county',
-              'Different house in the U.S., Different state',
-              'Abroad']
-
-    abs_data = get_demo_data('DP02',year,geo,variables,labels)
-
-    category_columns = abs_data.columns[2::]
-
-    perc_data = abs_data[category_columns].div(abs_data['Population 1 year and over'], axis=0)
-    perc_data = perc_data.multiply(100).round(1)
-
-    perc_data['ucgid'] = abs_data['ucgid']
-    
-    return perc_data
-
-# pop_data = get_total_pop('2022','county')
-# age_data = get_age_percent('2023','state')
-# race_data = get_race_percent('2023','state')
-
-# household_data = get_household_relationships('2022', 'state')
-
-# male_marital_data = get_male_marital_status('2020', 'state')
-# enrollment_data = get_education_level('2020', 'state')
-# veteran_data = get_veteran_status('2023', 'state')
-previous_residence_data = get_residence_year_ago('2023', 'state')
