4a83336a917c324337c383270f25dd4200625543
mark-NIH <mark.aronson@nih.gov>
2025-04-14 13:37:06 -0400

    fixing demo notebooks

--- ACS_retriever/__init__.py
+++ ACS_retriever/__init__.py
@@ -1,6 +1,7 @@
 # ACS_retriever/__init__.py
 
-from .DP02_functions import get_school_enrollment
+from .DP02_functions import get_school_enrollment, get_educational_attainment, get_male_marital_status
 from .DP05_functions import get_total_pop
 
-__all__ = ['get_school_enrollment','get_total_pop']
+__all__ = ['get_school_enrollment','get_educational_attainment',
+           'get_male_marital_status','get_total_pop']
