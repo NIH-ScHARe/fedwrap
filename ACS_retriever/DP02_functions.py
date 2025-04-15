51d0a9754bbca6dbe0630a7691566e7d1b1cc482
mark-NIH <mark.aronson@nih.gov>
2025-04-15 16:43:53 -0400

    add world region of birth, language spoken at home

--- ACS_retriever/DP02_functions.py
+++ ACS_retriever/DP02_functions.py
@@ -413,6 +413,62 @@ def get_veteran_status(year,geo,as_percent=False):
     
     return get_demo_data('DP02',year,geo,labels)
 
+def get_residence_year_ago(year,geo,as_percent=False):
+    
+    # set labels 
+    if not as_percent: 
+        if year in ['2009']:
+            labels = ['Number!!Estimate!!RESIDENCE 1 YEAR AGO!!Population 1 year and over!!Same house',
+                      'Number!!Estimate!!RESIDENCE 1 YEAR AGO!!Population 1 year and over!!Different house in the U.S.!!Same county',
+                      'Number!!Estimate!!RESIDENCE 1 YEAR AGO!!Population 1 year and over!!Different house in the U.S.!!Different county!!Same state',
+                      'Number!!Estimate!!RESIDENCE 1 YEAR AGO!!Population 1 year and over!!Different house in the U.S.!!Different county!!Different state',
+                      'Number!!Estimate!!RESIDENCE 1 YEAR AGO!!Population 1 year and over!!Abroad']
+        elif year in ['2010','2011','2012']:
+            labels = ['Estimate!!RESIDENCE 1 YEAR AGO!!Same house',
+                      'Estimate!!RESIDENCE 1 YEAR AGO!!Different house in the U.S.!!Same county',
+                      'Estimate!!RESIDENCE 1 YEAR AGO!!Different house in the U.S.!!Different county!!Same state',
+                      'Estimate!!RESIDENCE 1 YEAR AGO!!Different house in the U.S.!!Different county!!Different state',
+                      'Estimate!!RESIDENCE 1 YEAR AGO!!Abroad']
+        elif year in ['2013','2014','2015','2016','2017','2018','2019','2020','2021','2022','2023']:
+            labels = ['Estimate!!RESIDENCE 1 YEAR AGO!!Population 1 year and over!!Same house',
+                      'Estimate!!RESIDENCE 1 YEAR AGO!!Population 1 year and over!!Different house in the U.S.!!Same county',
+                      'Estimate!!RESIDENCE 1 YEAR AGO!!Population 1 year and over!!Different house in the U.S.!!Different county!!Same state',
+                      'Estimate!!RESIDENCE 1 YEAR AGO!!Population 1 year and over!!Different house in the U.S.!!Different county!!Different state',
+                      'Estimate!!RESIDENCE 1 YEAR AGO!!Population 1 year and over!!Abroad']
+        else:
+            print(f"Error: Unsupported year '{year}'")
+            return None
+    else:
+        if year in ['2009']:
+            labels = ['Percent!!Estimate!!RESIDENCE 1 YEAR AGO!!Population 1 year and over!!Same house',
+                      'Percent!!Estimate!!RESIDENCE 1 YEAR AGO!!Population 1 year and over!!Different house in the U.S.!!Same county',
+                      'Percent!!Estimate!!RESIDENCE 1 YEAR AGO!!Population 1 year and over!!Different house in the U.S.!!Different county!!Same state',
+                      'Percent!!Estimate!!RESIDENCE 1 YEAR AGO!!Population 1 year and over!!Different house in the U.S.!!Different county!!Different state',
+                      'Percent!!Estimate!!RESIDENCE 1 YEAR AGO!!Population 1 year and over!!Abroad']
+        elif year in ['2010','2011','2012']:
+            labels = ['Percent!!RESIDENCE 1 YEAR AGO!!Same house',
+                      'Percent!!RESIDENCE 1 YEAR AGO!!Different house in the U.S.!!Same county',
+                      'Percent!!RESIDENCE 1 YEAR AGO!!Different house in the U.S.!!Different county!!Same state',
+                      'Percent!!RESIDENCE 1 YEAR AGO!!Different house in the U.S.!!Different county!!Different state',
+                      'Percent!!RESIDENCE 1 YEAR AGO!!Abroad']
+        elif year in ['2013','2014','2015','2016','2019','2020','2021','2022','2023']:
+            labels = ['Percent!!RESIDENCE 1 YEAR AGO!!Population 1 year and over!!Same house',
+                      'Percent!!RESIDENCE 1 YEAR AGO!!Population 1 year and over!!Different house in the U.S.!!Same county',
+                      'Percent!!RESIDENCE 1 YEAR AGO!!Population 1 year and over!!Different house in the U.S.!!Different county!!Same state',
+                      'Percent!!RESIDENCE 1 YEAR AGO!!Population 1 year and over!!Different house in the U.S.!!Different county!!Different state',
+                      'Percent!!RESIDENCE 1 YEAR AGO!!Population 1 year and over!!Abroad']
+        elif year in ['2017','2018']:
+            labels = ['Percent Estimate!!RESIDENCE 1 YEAR AGO!!Population 1 year and over!!Same house',
+                      'Percent Estimate!!RESIDENCE 1 YEAR AGO!!Population 1 year and over!!Different house in the U.S.!!Same county',
+                      'Percent Estimate!!RESIDENCE 1 YEAR AGO!!Population 1 year and over!!Different house in the U.S.!!Different county!!Same state',
+                      'Percent Estimate!!RESIDENCE 1 YEAR AGO!!Population 1 year and over!!Different house in the U.S.!!Different county!!Different state',
+                      'Percent Estimate!!RESIDENCE 1 YEAR AGO!!Population 1 year and over!!Abroad']
+        else:
+            print(f"Error: Unsupported year '{year}'")
+            return None
+    
+    return get_demo_data('DP02',year,geo,labels)
+
 def get_place_of_birth(year,geo,as_percent=False):
     
     # set labels 
@@ -498,3 +554,138 @@ def get_US_citizenship_status(year,geo,as_percent=False):
     
     
     return get_demo_data('DP02',year,geo,labels)
+
+def get_world_region_of_birth_of_foreign_born(year,geo,as_percent=False):
+    
+    # set labels 
+    if not as_percent:
+        if year in ['2009']:
+            labels = ['Number!!Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Europe',
+                      'Number!!Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Asia',
+                      'Number!!Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Africa',
+                      'Number!!Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Oceania',
+                      'Number!!Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Latin America',
+                      'Number!!Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Northern America']
+        elif year in ['2010','2011','2012']:
+            labels = ['Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Europe',
+                      'Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Asia',
+                      'Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Africa',
+                      'Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Oceania',
+                      'Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Latin America',
+                      'Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Northern America']
+        elif year in ['2013','2014','2015','2016','2017','2018','2019','2020','2021','2022']:
+            labels = ['Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Europe',
+                      'Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Asia',
+                      'Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Africa',
+                      'Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Oceania',
+                      'Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Latin America',
+                      'Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Northern America']
+        elif year in ['2023']:
+            labels = ['Estimate!!WORLD REGION OF BIRTH OF FOREIGN-BORN!!Foreign-born population, excluding population born at sea!!Europe',
+                      'Estimate!!WORLD REGION OF BIRTH OF FOREIGN-BORN!!Foreign-born population, excluding population born at sea!!Asia',
+                      'Estimate!!WORLD REGION OF BIRTH OF FOREIGN-BORN!!Foreign-born population, excluding population born at sea!!Africa',
+                      'Estimate!!WORLD REGION OF BIRTH OF FOREIGN-BORN!!Foreign-born population, excluding population born at sea!!Oceania',
+                      'Estimate!!WORLD REGION OF BIRTH OF FOREIGN-BORN!!Foreign-born population, excluding population born at sea!!Latin America',
+                      'Estimate!!WORLD REGION OF BIRTH OF FOREIGN-BORN!!Foreign-born population, excluding population born at sea!!Northern America']
+        else:
+            print(f"Error: Unsupported year '{year}'")
+            return None
+    else:
+        if year in ['2009']:
+            labels = ['Percent!!Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Europe',
+                      'Percent!!Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Asia',
+                      'Percent!!Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Africa',
+                      'Percent!!Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Oceania',
+                      'Percent!!Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Latin America',
+                      'Percent!!Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Northern America']
+        elif year in ['2010','2011','2012']:
+            labels = ['Percent!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Europe',
+                      'Percent!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Asia',
+                      'Percent!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Africa',
+                      'Percent!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Oceania',
+                      'Percent!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Latin America',
+                      'Percent!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Northern America']
+        elif year in ['2013','2014','2015','2016','2019','2020','2021','2022']:
+            labels = ['Percent!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Europe',
+                      'Percent!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Asia',
+                      'Percent!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Africa',
+                      'Percent!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Oceania',
+                      'Percent!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Latin America',
+                      'Percent!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Northern America']
+        elif year in ['2017','2018']:
+            labels = ['Percent Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Europe',
+                      'Percent Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Asia',
+                      'Percent Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Africa',
+                      'Percent Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Oceania',
+                      'Percent Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Latin America',
+                      'Percent Estimate!!WORLD REGION OF BIRTH OF FOREIGN BORN!!Foreign-born population, excluding population born at sea!!Northern America']
+        elif year in ['2023']:
+            labels = ['Percent!!WORLD REGION OF BIRTH OF FOREIGN-BORN!!Foreign-born population, excluding population born at sea!!Europe',
+                      'Percent!!WORLD REGION OF BIRTH OF FOREIGN-BORN!!Foreign-born population, excluding population born at sea!!Asia',
+                      'Percent!!WORLD REGION OF BIRTH OF FOREIGN-BORN!!Foreign-born population, excluding population born at sea!!Africa',
+                      'Percent!!WORLD REGION OF BIRTH OF FOREIGN-BORN!!Foreign-born population, excluding population born at sea!!Oceania',
+                      'Percent!!WORLD REGION OF BIRTH OF FOREIGN-BORN!!Foreign-born population, excluding population born at sea!!Latin America',
+                      'Percent!!WORLD REGION OF BIRTH OF FOREIGN-BORN!!Foreign-born population, excluding population born at sea!!Northern America']
+        else:
+            print(f"Error: Unsupported year '{year}'")
+            return None
+    
+    return get_demo_data('DP02',year,geo,labels)
+
+def get_language_spoken_at_home(year,geo,as_percent=False):
+    
+    # set labels 
+    if not as_percent:
+        if year in ['2009']:
+            labels = ['Number!!Estimate!!LANGUAGE SPOKEN AT HOME!!Population 5 years and over!!English only',
+                      'Number!!Estimate!!LANGUAGE SPOKEN AT HOME!!Population 5 years and over!!Spanish',
+                      'Number!!Estimate!!LANGUAGE SPOKEN AT HOME!!Population 5 years and over!!Other Indo-European languages',
+                      'Number!!Estimate!!LANGUAGE SPOKEN AT HOME!!Population 5 years and over!!Asian and Pacific Islander languages',
+                      'Number!!Estimate!!LANGUAGE SPOKEN AT HOME!!Population 5 years and over!!Other languages']
+        elif year in ['2010','2011','2012']:
+            labels = ['Estimate!!LANGUAGE SPOKEN AT HOME!!English only',
+                      'Estimate!!LANGUAGE SPOKEN AT HOME!!Language other than English!!Spanish',
+                      'Estimate!!LANGUAGE SPOKEN AT HOME!!Language other than English!!Other Indo-European languages',
+                      'Estimate!!LANGUAGE SPOKEN AT HOME!!Language other than English!!Asian and Pacific Islander languages',
+                      'Estimate!!LANGUAGE SPOKEN AT HOME!!Language other than English!!Other languages']
+        elif year in ['2013','2014','2015','2016','2017','2018','2019','2020','2021','2022','2023']:
+            labels = ['Estimate!!LANGUAGE SPOKEN AT HOME!!Population 5 years and over!!English only',
+                      'Estimate!!LANGUAGE SPOKEN AT HOME!!Population 5 years and over!!Spanish',
+                      'Estimate!!LANGUAGE SPOKEN AT HOME!!Population 5 years and over!!Other Indo-European languages',
+                      'Estimate!!LANGUAGE SPOKEN AT HOME!!Population 5 years and over!!Asian and Pacific Islander languages',
+                      'Estimate!!LANGUAGE SPOKEN AT HOME!!Population 5 years and over!!Other languages']
+        else:
+            print(f"Error: Unsupported year '{year}'")
+            return None
+    
+    else:
+        if year in ['2009']:
+            labels = ['Percent!!Estimate!!LANGUAGE SPOKEN AT HOME!!Population 5 years and over!!English only',
+                      'Percent!!Estimate!!LANGUAGE SPOKEN AT HOME!!Population 5 years and over!!Spanish',
+                      'Percent!!Estimate!!LANGUAGE SPOKEN AT HOME!!Population 5 years and over!!Other Indo-European languages',
+                      'Percent!!Estimate!!LANGUAGE SPOKEN AT HOME!!Population 5 years and over!!Asian and Pacific Islander languages',
+                      'Percent!!Estimate!!LANGUAGE SPOKEN AT HOME!!Population 5 years and over!!Other languages']
+        elif year in ['2010','2011','2012']:
+            labels = ['Percent!!LANGUAGE SPOKEN AT HOME!!English only',
+                      'Percent!!LANGUAGE SPOKEN AT HOME!!Language other than English!!Spanish',
+                      'Percent!!LANGUAGE SPOKEN AT HOME!!Language other than English!!Other Indo-European languages',
+                      'Percent!!LANGUAGE SPOKEN AT HOME!!Language other than English!!Asian and Pacific Islander languages',
+                      'Percent!!LANGUAGE SPOKEN AT HOME!!Language other than English!!Other languages']
+        elif year in ['2013','2014','2015','2016','2019','2020','2021','2022','2023']:
+            labels = ['Percent!!LANGUAGE SPOKEN AT HOME!!Population 5 years and over!!English only',
+                      'Percent!!LANGUAGE SPOKEN AT HOME!!Population 5 years and over!!Spanish',
+                      'Percent!!LANGUAGE SPOKEN AT HOME!!Population 5 years and over!!Other Indo-European languages',
+                      'Percent!!LANGUAGE SPOKEN AT HOME!!Population 5 years and over!!Asian and Pacific Islander languages',
+                      'Percent!!LANGUAGE SPOKEN AT HOME!!Population 5 years and over!!Other languages']
+        elif year in ['2017','2018']:
+            labels = ['Percent Estimate!!LANGUAGE SPOKEN AT HOME!!Population 5 years and over!!English only',
+                      'Percent Estimate!!LANGUAGE SPOKEN AT HOME!!Population 5 years and over!!Spanish',
+                      'Percent Estimate!!LANGUAGE SPOKEN AT HOME!!Population 5 years and over!!Other Indo-European languages',
+                      'Percent Estimate!!LANGUAGE SPOKEN AT HOME!!Population 5 years and over!!Asian and Pacific Islander languages',
+                      'Percent Estimate!!LANGUAGE SPOKEN AT HOME!!Population 5 years and over!!Other languages']
+        else:
+            print(f"Error: Unsupported year '{year}'")
+            return None
+    
+    return get_demo_data('DP02',year,geo,labels)
+        
--- ACS_retriever/__init__.py
+++ ACS_retriever/__init__.py
@@ -1,7 +1,8 @@
 # ACS_retriever/__init__.py
 
-from .DP02_functions import get_school_enrollment, get_educational_attainment, get_male_marital_status
+from .DP02_functions import get_school_enrollment, get_educational_attainment, get_male_marital_status, get_world_region_of_birth_of_foreign_born, get_language_spoken_at_home
 from .DP05_functions import get_total_pop
 
 __all__ = ['get_school_enrollment','get_educational_attainment',
-           'get_male_marital_status','get_total_pop']
+           'get_male_marital_status','get_world_region_of_birth_of_foreign_born',
+           'get_language_spoken_at_home', 'get_total_pop']
